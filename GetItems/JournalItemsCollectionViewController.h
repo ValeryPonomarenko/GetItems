aec64684744a7b308858fc9498f45192cbf2e52e
Valery Ponomarenko <ponomarenko@valerys-macbook-pro.local>
2016-05-10 11:52:07 +0300

    CollectionView was added

---
+++ GetItems/JournalItemsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  JournalItemsCollectionViewController.h
+//  GetItems
+//
+//  Created by Valery Ponomarenko on 10/05/16.
+//  Copyright © 2016 Valery Ponomarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JournalItemsCollectionViewController : UICollectionViewController
+
+@end
